33f09a73e89d3013210e873b71619e556d39bbab
Miguel Huerta Vázquez <fulgrim1@outlook.es>
2025-07-19 04:24:59 -0600

    PENDIENTES

--- Game1.cs
+++ Game1.cs
@@ -3,11 +3,26 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace ProyectT
+
+    //PENDIENTES*
+    //GENERAR SPRITES (VILLANO, CRIATURA, PERSONAJE PRINCIPAL, 2 HIJOS.
+
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        //Declaración de variable para definir los sprites del proyecto que vamos a cargar en el metodo 
+        //load content
+
+        //TEXTURE 2D SE REFIERE A UNA IMAGEN BI-DIMENSIONAL 
+        Texture2D targetSprite;
+        //Inyecto mi primer sprite
+        Texture2D Main;
+        Texture2D backgroundSprite;
+
+
 
         public Game1()
         {
@@ -23,11 +38,17 @@ namespace ProyectT
             base.Initialize();
         }
 
+
+        //ESTE METODO CARGA EL CONTENIDO QUE LLAMEMOS POR LA VARIABLE, EN ESTE CASO VA A LLAMAR Texture2D
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            Texture2D texture2D = Content.Load<Texture2D>("Fonddemo");
+            
+
+
         }
 
         protected override void Update(GameTime gameTime)
